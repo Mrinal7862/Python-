94a906993bd3cfca7c3e036aa22256eb635e9c52
Mrinal7862 <codemaxter473@gmail.com>
2025-03-10 03:02:17 +0530

    secondString

---
+++ strings_2.py
@@ -0,0 +1,32 @@
+# Upper case
+txt = "my name is maxter"
+print(txt.upper())
+
+# Lower Case
+nlower = "WELCOME TO MY WEB DEVELOPMENT COURSE"
+print(nlower.lower())
+
+# strip
+arr = "        the ocasion is important       "
+print(arr.strip())
+
+# split
+text2 = "I'm the only one who need to practice more."
+print(text2.split())
+# replace
+txt = txt.replace("maxter", "matrix")
+print(txt)
+# concatenation
+statement1 = "My qualifications are"
+statement2 = "10th", "12th", "B.tech (AI/ML)"
+# print(statement1 + " " + statement2)
+# print(type(statement2))
+# format strings
+# print("Hello My name is Mrinal Devnath, and {} {}".format(statement1, statement2))
+# f-string method
+# print(f"Sachin sir and Ayan sir Both are proficient Teachers of my institute, and {statement1} {statement2}")
+# \n
+# name = "My name is Mrinal Devnath"
+# print(name, "\rWelcome to my web development course")
+
+# methods
