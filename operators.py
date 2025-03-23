9d7cceb0bd28c71ea205356c722bfea82d69a3ce
Mrinal7862 <codemaxter473@gmail.com>
2025-03-24 04:40:17 +0530

    python_new_update

---
+++ operators.py
@@ -0,0 +1,43 @@
+# # Arithmetic operators
+# # + , - , *, /, %, **
+#
+# # print(3+2)
+# # print(9-5)
+# # print(2*2)
+# # print(8/9)
+# # print(4%3)
+# # print(3**20)
+#
+#
+# # Assignment operator
+#
+# # = , +=, -=, *=, /=, %=
+# num = 2
+# print(num)
+#
+# num += 3 # num(1) 1 += 3  1 + 3
+#
+# print(num)
+#
+# num -= 5
+# print(num)
+#
+# num += 20
+# num *= 3
+# print(num)
+#
+# # /=
+# num /= 2
+# print(int(num))
+
+
+# Comparison operators
+print(3==5)
+# ==, <=, >=, <, > !=
+num1 = 8
+num2 = 3
+print(num2 < num1)
+# Logical operators
+# Identity operators
+# Membership operators
+# Bitwise operators
